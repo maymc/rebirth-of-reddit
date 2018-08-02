a8577409843982bcff100aaf766f9f229fd63dfd
May Chen <maychen@Mays-MacBook-Pro.local>
2018-08-01 22:00:58 -1000

    Changed url to be consistent between all articles. Only one subreddit rather than different kinds

--- js/app.js
+++ js/app.js
@@ -11,98 +11,92 @@ function authorInfo(articleNumber, username) {
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 //First reddit article
-const url1 = "https://www.reddit.com/r/rarepuppers/comments/91ggsp/munch_chomp_nom.json";
+const url = "https://www.reddit.com/r/rarepuppers.json";
 
 
-const request1 = (url1, callback) => {
+const request1 = (url, callback) => {
   let oReq = new XMLHttpRequest();
   oReq.addEventListener("load", callback);
-  oReq.open("GET", url1);
+  oReq.open("GET", url);
   oReq.send();
 };
 
-request1(url1, res => {
+request1(url, res => {
   const myObj = JSON.parse(res.currentTarget.response);
   console.log("response: ", myObj);
-  console.log("articleTitle: ", myObj[0].data.children[0].data.title);
-  console.log("articleAuthor: ", myObj[0].data.children[0].data.author);
+  console.log("articleTitle: ", myObj.data.children[15].data.title);
+  console.log("articleAuthor: ", myObj.data.children[15].data.author);
 
   //Put info into html file
-  document.getElementById("article1").innerHTML = myObj[0].data.children[0].data.title;
+  document.getElementById("article1").innerHTML = myObj.data.children[15].data.title;
 
-  authorInfo(1, myObj[0].data.children[0].data.author);
+  authorInfo(1, myObj.data.children[15].data.author);
 
 })
 
 
 
 //Second reddit article
-const url2 = "https://www.reddit.com/r/rarepuppers/comments/90z3pu/nope_dis_mine.json";
-
-const request2 = (url2, callback) => {
+const request2 = (url, callback) => {
   let oReq = new XMLHttpRequest();
   oReq.addEventListener("load", callback);
-  oReq.open("GET", url2);
+  oReq.open("GET", url);
   oReq.send();
 };
 
-request2(url2, res => {
+request2(url, res => {
   const myObj = JSON.parse(res.currentTarget.response);
   console.log("response: ", myObj);
-  console.log("articleTitle: ", myObj[0].data.children[0].data.title);
-  console.log("articleAuthor: ", myObj[0].data.children[0].data.author);
+  console.log("articleTitle: ", myObj.data.children[16].data.title);
+  console.log("articleAuthor: ", myObj.data.children[16].data.author);
 
   //Put info into html file
-  document.getElementById("article2").innerHTML = myObj[0].data.children[0].data.title;
+  document.getElementById("article2").innerHTML = myObj.data.children[16].data.title;
 
-  authorInfo(2, myObj[0].data.children[0].data.author);
+  authorInfo(2, myObj.data.children[16].data.author);
 
 })
 
 
 
 //Third reddit article
-const url3 = "https://www.reddit.com/user/AnimalsWorld410/comments/8fue9r/its_bath_time_for_dogs_try_not_to_laugh_animals_hd.json";
-
-const request3 = (url3, callback) => {
+const request3 = (url, callback) => {
   let oReq = new XMLHttpRequest();
   oReq.addEventListener("load", callback);
-  oReq.open("GET", url3);
+  oReq.open("GET", url);
   oReq.send();
 };
 
-request3(url3, res => {
+request3(url, res => {
   const myObj = JSON.parse(res.currentTarget.response);
   console.log("response: ", myObj);
-  console.log("articleTitle: ", myObj[0].data.children[0].data.title);
-  console.log("articleAuthor: ", myObj[0].data.children[0].data.author);
+  console.log("articleTitle: ", myObj.data.children[18].data.title);
+  console.log("articleAuthor: ", myObj.data.children[18].data.author);
 
   //Put info into html file
-  document.getElementById("article3").innerHTML = myObj[0].data.children[0].data.title;
+  document.getElementById("article3").innerHTML = myObj.data.children[18].data.title;
 
-  authorInfo(3, myObj[0].data.children[0].data.author);
+  authorInfo(3, myObj.data.children[18].data.author);
 
 })
 
 //Fourth reddit article
-const url4 = "https://www.reddit.com/r/aww/comments/67vyjn/oh_good_morning_how_may_i_help_you.json";
-
-const request4 = (url4, callback) => {
+const request4 = (url, callback) => {
   let oReq = new XMLHttpRequest();
   oReq.addEventListener("load", callback);
-  oReq.open("GET", url4);
+  oReq.open("GET", url);
   oReq.send();
 };
 
-request3(url4, res => {
+request4(url, res => {
   const myObj = JSON.parse(res.currentTarget.response);
   console.log("response: ", myObj);
-  console.log("articleTitle: ", myObj[0].data.children[0].data.title);
-  console.log("articleAuthor: ", myObj[0].data.children[0].data.author);
+  console.log("articleTitle: ", myObj.data.children[20].data.title);
+  console.log("articleAuthor: ", myObj.data.children[20].data.author);
 
   //Put info into html file
-  document.getElementById("article4").innerHTML = myObj[0].data.children[0].data.title;
+  document.getElementById("article4").innerHTML = myObj.data.children[20].data.title;
 
-  authorInfo(4, myObj[0].data.children[0].data.author);
+  authorInfo(4, myObj.data.children[20].data.author);
 
 })
