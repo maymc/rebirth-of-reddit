75ec03d01302c0008e7b39af93c4365688d044f4
May Chen <maychen@Mays-MacBook-Pro.local>
2018-08-03 00:50:51 -1000

    Cleaned up code a little. Deleted commented out code

--- js/app.js
+++ js/app.js
@@ -1,13 +1,5 @@
 //Helper Functions:
 
-//Create an author, show post age, num views
-function authorInfo(username, upsCount, numComments) {
-  let authorElem = document.createElement("div");
-  authorElem.className = "author";
-  authorElem.innerHTML = "by " + username + " | Ups: " + upsCount + " | Comments: " + numComments;
-  articleNum.appendChild(authorElem);
-}
-
 const request = (url, callback) => {
   let oReq = new XMLHttpRequest();
   oReq.addEventListener("load", callback);
@@ -41,7 +33,7 @@ request(url, res => {
     let username = myObj.data.children[i].data.author;
 
     console.log("post ups: ", myObj.data.children[i].data.upsCount);
-    let postUps = myObj.data.children[i].data.author;
+    let postUps = myObj.data.children[i].data.ups;
 
     console.log("post comment count: ", myObj.data.children[i].data.num_comments);
     let numComments = myObj.data.children[i].data.num_comments;
@@ -85,92 +77,9 @@ request(url, res => {
     titleElem.innerHTML = postTitle;
     postElem.appendChild(titleElem);
 
-    authorElem.innerHTML = "by " + username + " | Ups: " + postUps + " | Comments: " + numComments;
+    authorElem.innerHTML = "by " + username + " • Ups: " + postUps + " • Comments: " + numComments;
     postElem.appendChild(authorElem);
-
-
-
-    // authorInfo(i, myObj.data.children[6].data.author, myObj.data.children[6].data.ups, myObj.data.children[6].data.num_comments);
 
     postsBodyElem.appendChild(postElem);
   }
 })
-
-// //Second reddit article
-// request(url, res => {
-//   const myObj = JSON.parse(res.currentTarget.response);
-//   console.log("response: ", myObj);
-//   console.log("articleTitle: ", myObj.data.children[12].data.title);
-//   console.log("articleAuthor: ", myObj.data.children[12].data.author);
-//   console.log("media link: ", myObj.data.children[12].data.url);
-//   let mediaLink = myObj.data.children[12].data.url;
-//   // console.log("test: ", imgLink[imgLink.length - 1]);
-
-//   //Put info into html file
-//   if (myObj.data.children[12].data.post_hint === "link") {
-//     request(mediaLink, res => {
-//       let articleElem = document.getElementById("article2");
-//       let imageElem = document.createElement("img");
-
-//       //If this is a gif and it has a 'v' at the end of the link, remove the v for the gif to load properly on the page
-//       if (mediaLink[mediaLink.length - 1] === "v") {
-//         let gifLink = mediaLink.slice(0, length - 1);
-//         console.log("gifLink: ", gifLink);
-//         imageElem.src = gifLink;
-//         imageElem.alt = "teacup puppy";
-//         articleElem.appendChild(imageElem);
-//       }
-//     })
-//   }
-
-//   document.getElementById("article2").innerHTML = myObj.data.children[12].data.title;
-
-//   authorInfo(2, myObj.data.children[12].data.author, myObj.data.children[12].data.ups, myObj.data.children[12].data.num_comments);
-
-// })
-
-// //Third reddit article
-// request(url, res => {
-//   const myObj = JSON.parse(res.currentTarget.response);
-//   console.log("response: ", myObj);
-//   console.log("articleTitle: ", myObj.data.children[9].data.title);
-//   console.log("articleAuthor: ", myObj.data.children[9].data.author);
-//   console.log("media link: ", myObj.data.children[9].data.url);
-//   let mediaLink = myObj.data.children[9].data.url;
-
-//   //Put info into html file
-//   if (myObj.data.children[9].data.post_hint === "link") {
-//     request(mediaLink, () => {
-//       let articleElem = document.getElementById("article3");
-//       let imageElem = document.createElement("img");
-
-//       //If this is a gif and it has a 'v' at the end of the link, remove the v for the gif to load properly on the page
-//       if (mediaLink[mediaLink.length - 1] === "v") {
-//         let gifLink = mediaLink.slice(0, length - 1);
-//         console.log("gifLink: ", gifLink);
-//         imageElem.src = gifLink;
-//         imageElem.alt = "doggy kisses";
-//         articleElem.appendChild(imageElem);
-//       }
-//     })
-//   }
-
-//   document.getElementById("article3").innerHTML = myObj.data.children[9].data.title;
-
-//   authorInfo(3, myObj.data.children[9].data.author, myObj.data.children[9].data.ups, myObj.data.children[9].data.num_comments);
-
-// })
-
-// //Fourth reddit article
-// request(url, res => {
-//   const myObj = JSON.parse(res.currentTarget.response);
-//   console.log("response: ", myObj);
-//   console.log("articleTitle: ", myObj.data.children[21].data.title);
-//   console.log("articleAuthor: ", myObj.data.children[21].data.author);
-
-//   //Put info into html file
-//   document.getElementById("article4").innerHTML = myObj.data.children[21].data.title;
-
-//   authorInfo(4, myObj.data.children[21].data.author, myObj.data.children[21].data.ups, myObj.data.children[21].data.num_comments);
-
-// })
